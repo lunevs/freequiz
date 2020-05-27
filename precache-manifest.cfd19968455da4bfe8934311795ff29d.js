d7bb5db0b18311b41c9aa02c8bd403f538a89c93
Сергей Лунев <smlunev@ichtusMacBook.local>
2020-05-27 12:25:19 +0300

    Updates

---
+++ precache-manifest.cfd19968455da4bfe8934311795ff29d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35e00ea08f1fd3539c1686ffcc3ef20",
+    "url": "/freequiz/index.html"
+  },
+  {
+    "revision": "6765984d8e92693daee2",
+    "url": "/freequiz/static/js/2.856b71e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/freequiz/static/js/2.856b71e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd4157b9fdffeff783c",
+    "url": "/freequiz/static/js/main.a134d531.chunk.js"
+  },
+  {
+    "revision": "4c34e92f7975a2cd46c3",
+    "url": "/freequiz/static/js/runtime-main.e30611f3.js"
+  }
+]);
